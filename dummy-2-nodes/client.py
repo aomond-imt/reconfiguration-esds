3424f8c14f9a70a392d89fc23119129ecb418061
aomond <antoine.omond@imt-atlantique.fr>
2023-07-24 20:43:14 +0200

    dummy

--- dummy-2-nodes/client.py
+++ dummy-2-nodes/client.py
@@ -2,6 +2,7 @@
 import json
 
 from esds.node import Node
+from esds.plugins.power_states import PowerStatesComms
 
 
 # def execute(api: Node):
@@ -11,9 +12,12 @@ from esds.node import Node
 
 
 def execute(api: Node):
-    # uptimes = json.load(open("node_scenarios.json"))
-    api.turn_off()
+    sending_cons = PowerStatesComms(api)
+    sending_cons.set_power("eth0", 0, 0.1, 0.1)
+    api.turn_on()
     # api.log(f"Starting client node {api.node_id}")
+    api.sendt("eth0", "zefoij", 60, 1, timeout=60)
     api.wait(500)
     # api.sendt("eth0","Hello World!",1,1, 50)
     api.turn_off()
+    sending_cons.report_energy()
