23726833b4672a5c228c7a01df12b30cee78160d
aomond <antoine.omond@imt-atlantique.fr>
2023-08-27 12:14:00 +0200

    Run expe and stop expe on node updated scripts

--- stop_expes_on_nodes.sh
+++ stop_expes_on_nodes.sh
@@ -19,7 +19,6 @@ hosts_length=${#hosts[@]}
 # use for loop to read all values and indexes
 for (( i=0; i<${hosts_length}; i++ ));
 do
-  echo "starting on host ${hosts[$i]}"
+  echo "killing tmux session on host ${hosts[$i]}"
   ssh ${hosts[$i]} "tmux kill-session -t $i"
-  ssh ${hosts[$i]} "tmux new-session -d -s $i 'cd ~/reconfiguration-esds && source venv/bin/activate && python3 run_concerto_d_expes.py'"
 done
