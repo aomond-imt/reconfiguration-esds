0859e6735e2af25ef9d64c1beeff613e9b60c39f
aomond <antoine.omond@imt-atlantique.fr>
2023-08-28 12:50:24 +0200

    Add option to check sweeper state details

--- check_sweeper_states.py
+++ check_sweeper_states.py
@@ -1,10 +1,27 @@
 from execo_engine import ParamSweeper
 import os
+import sys
+
+args = sys.argv
+if len(args) > 1:
+    detail = True
+else:
+    detail = False
+
 
 for i in range(5):
     sweeper_path = f"concerto-d-results/{i}/sweeper"
     if os.path.exists(sweeper_path) and os.path.getsize(f"{sweeper_path}/sweeps") > 0:
         print(f"Sweeper {i}: ", end="")
-        print(ParamSweeper(sweeper_path))
+        sweeper = ParamSweeper(sweeper_path)
+        print(sweeper)
+        if detail:
+            print("In progress:")
+            for ip in sweeper.get_inprogress():
+                print(f"  - {ip}")
+            print("Remaining:")
+            for r in sweeper.get_remaining():
+                print(f"  - {r}")
+
     else:
         print(f"Sweeper {i}: no experiment done")
